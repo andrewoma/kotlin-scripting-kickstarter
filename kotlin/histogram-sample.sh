a568828f507c18bca4c783601cc89c988d3bcee0
Daniel Mabbett <triumph_2500@hotmail.com>
2014-04-14 21:53:40 +1000

    Determine the current scripts directory in a more reliable and cross platform way

--- kotlin/histogram-sample.sh
+++ kotlin/histogram-sample.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-data="$0/../../test/resources/histogram-sample.txt"
-histogram.kt -input "$data" -output "$HOME/histogram-sample.png" -label 'Response (s)'
+currentDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+output="${HOME}/histogram-sample.jpg"
+
+data="${currentDir}/../test/resources/histogram-sample.txt"
+histogram.kt -input "$data" -output "${output}" -label 'Response (s)'
+
+echo "Histogram saved to '${output}'"
